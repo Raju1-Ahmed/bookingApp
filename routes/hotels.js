c8e57dd368cfec0181e2db0ae43a33f0fef3b340
Raju1-Ahmed <robiussaniraju@gmail.com>
2023-07-09 03:38:28 +0600

    authentication login register

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,61 +1,23 @@
 import express from "express"
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 const router = express.Router();
 
-//create
-router.post("/", async (req, res) => {
-    const newHotel = new Hotel(req.body);
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+//CREATE
+router.post("/", createHotel)
 
-//update
-router.put("/:id", async (req, res) => {
-    try {
-        const updateHotel = await Hotel.findByIdAndUpdate(
-            req.params.id,
-            { $set: req.body },
-            { new: true })
-        res.status(200).json(updateHotel)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+//UPDATE
+router.put("/:id", updateHotel)
 
-//deleted
-router.delete("/:id", async (req, res) => {
-    try {
-        await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json("Hotel han been deleted.")
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+//DELETE
+router.delete("/:id", deleteHotel)
 
-//Get By Id
-router.get("/:id", async (req, res) => {
-    try {
-        const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+//GET
+router.get("/:id", getHotel)
 
-//Get all Hotels
-router.get("/", async (req, res, next) => {
-    try {
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels)
-    } catch (err) {
-        next(err)
-    }
-})
+//GET ALL
+router.get("/", getHotels)
 
 
 
